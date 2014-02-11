f03d6dc67fe2359843244b9f7532036ec83ce0ca
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2014-02-11 10:31:28 +0800

    a

--- src/com/hoyoji/hoyoji/models/MoneyExpense.java
+++ src/com/hoyoji/hoyoji/models/MoneyExpense.java
@@ -12,21 +12,20 @@ import com.hoyoji.android.hyjframework.HyjModelEditor;
 import com.hoyoji.hoyoji.R;
 
 @Table(name = "MoneyExpense", id = BaseColumns._ID)
-
 public class MoneyExpense extends HyjModel{
 
 	@Column(name = "id", index = true, unique = true)
 	private String mId;
 	
-	@Column(name = "amount")
-	private Double mAmount;
-	
 	@Column(name = "pictureId")
-	private Double mPictureId;
+	private String mPictureId;
 	
 	@Column(name = "date")
 	private String mDate;
 
+	@Column(name = "amount")
+	private Double mAmount;
+	
 	@Column(name = "expenseType")
 	private String mExpenseType;
 
@@ -36,7 +35,23 @@ public class MoneyExpense extends HyjModel{
 	@Column(name = "localFriendId")
 	private String mLocalFriendId;
 
+	@Column(name = "friendAccountId")
+	private String mFriendAccountId;
+
+	@Column(name = "moneyAccountId")
+	private String mMoneyAccountId;
 	
+	@Column(name = "projectId")
+	private String mProjectId;
+	
+	@Column(name = "moneyExpenseCategory")
+	private String mMoneyExpenseCategory;
+	
+	@Column(name = "exchangeRate")
+	private Double mExchangeRate;
+
+	@Column(name = "remark")
+	private String mRemark;
 	
 	@Column(name = "serverRecordHash")
 	private String mServerRecordHash;
@@ -63,7 +78,7 @@ public class MoneyExpense extends HyjModel{
 	private String mGeoLat;
 	
 	@Column(name = "address")
-	private String mAddress;
+	private String mAddress;	
 	
 	@Override
 	public void validate(HyjModelEditor hyjModelEditor) {
@@ -78,6 +93,174 @@ public class MoneyExpense extends HyjModel{
 
 	public void setId(String mId) {
 		this.mId = mId;
+	}
+
+	public String getmPictureId() {
+		return mPictureId;
+	}
+
+	public void setmPictureId(String mPictureId) {
+		this.mPictureId = mPictureId;
+	}
+
+	public String getmDate() {
+		return mDate;
+	}
+
+	public void setmDate(String mDate) {
+		this.mDate = mDate;
+	}
+
+	public Double getmAmount() {
+		return mAmount;
+	}
+
+	public void setmAmount(Double mAmount) {
+		this.mAmount = mAmount;
+	}
+	
+	public String getmExpenseType() {
+		return mExpenseType;
+	}
+
+	public void setmExpenseType(String mExpenseType) {
+		this.mExpenseType = mExpenseType;
+	}
+
+	public String getmFriendUserId() {
+		return mFriendUserId;
+	}
+
+	public void setmFriendUserId(String mFriendUserId) {
+		this.mFriendUserId = mFriendUserId;
+	}
+
+	public String getmLocalFriendId() {
+		return mLocalFriendId;
+	}
+
+	public void setmLocalFriendId(String mLocalFriendId) {
+		this.mLocalFriendId = mLocalFriendId;
+	}
+
+	public String getmFriendAccountId() {
+		return mFriendAccountId;
+	}
+
+	public void setmFriendAccountId(String mFriendAccountId) {
+		this.mFriendAccountId = mFriendAccountId;
+	}
+
+	public String getmMoneyAccountId() {
+		return mMoneyAccountId;
+	}
+
+	public void setmMoneyAccountId(String mMoneyAccountId) {
+		this.mMoneyAccountId = mMoneyAccountId;
+	}
+
+	public String getmProjectId() {
+		return mProjectId;
+	}
+
+	public void setmProjectId(String mProjectId) {
+		this.mProjectId = mProjectId;
+	}
+
+	public String getmMoneyExpenseCategory() {
+		return mMoneyExpenseCategory;
+	}
+
+	public void setmMoneyExpenseCategory(String mMoneyExpenseCategory) {
+		this.mMoneyExpenseCategory = mMoneyExpenseCategory;
+	}
+
+	public Double getmExchangeRate() {
+		return mExchangeRate;
+	}
+
+	public void setmExchangeRate(Double mExchangeRate) {
+		this.mExchangeRate = mExchangeRate;
+	}
+
+	public String getmRemark() {
+		return mRemark;
+	}
+
+	public void setmRemark(String mRemark) {
+		this.mRemark = mRemark;
+	}
+
+	public String getmServerRecordHash() {
+		return mServerRecordHash;
+	}
+
+	public void setmServerRecordHash(String mServerRecordHash) {
+		this.mServerRecordHash = mServerRecordHash;
+	}
+
+	public String getmLastServerUpdateTime() {
+		return mLastServerUpdateTime;
+	}
+
+	public void setmLastServerUpdateTime(String mLastServerUpdateTime) {
+		this.mLastServerUpdateTime = mLastServerUpdateTime;
+	}
+
+	public String getmLastClientUpdateTime() {
+		return mLastClientUpdateTime;
+	}
+
+	public void setmLastClientUpdateTime(String mLastClientUpdateTime) {
+		this.mLastClientUpdateTime = mLastClientUpdateTime;
+	}
+
+	public String getmLastSyncTime() {
+		return mLastSyncTime;
+	}
+
+	public void setmLastSyncTime(String mLastSyncTime) {
+		this.mLastSyncTime = mLastSyncTime;
+	}
+
+	public String getmOwnerUserId() {
+		return mOwnerUserId;
+	}
+
+	public void setmOwnerUserId(String mOwnerUserId) {
+		this.mOwnerUserId = mOwnerUserId;
+	}
+
+	public String getmLocation() {
+		return mLocation;
+	}
+
+	public void setmLocation(String mLocation) {
+		this.mLocation = mLocation;
+	}
+
+	public String getmGeoLon() {
+		return mGeoLon;
+	}
+
+	public void setmGeoLon(String mGeoLon) {
+		this.mGeoLon = mGeoLon;
+	}
+
+	public String getmGeoLat() {
+		return mGeoLat;
+	}
+
+	public void setmGeoLat(String mGeoLat) {
+		this.mGeoLat = mGeoLat;
+	}
+
+	public String getmAddress() {
+		return mAddress;
+	}
+
+	public void setmAddress(String mAddress) {
+		this.mAddress = mAddress;
 	}
 
 }
