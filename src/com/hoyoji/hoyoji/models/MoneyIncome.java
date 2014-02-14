c4b97a45510b145aa163ed417d3b567236050c95
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2014-02-14 16:07:23 +0800

    a

---
+++ src/com/hoyoji/hoyoji/models/MoneyIncome.java
@@ -0,0 +1,332 @@
+package com.hoyoji.hoyoji.models;
+
+import java.util.UUID;
+
+import android.provider.BaseColumns;
+
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.hoyoji.android.hyjframework.HyjModel;
+import com.hoyoji.android.hyjframework.HyjModelEditor;
+import com.hoyoji.hoyoji.R;
+
+@Table(name = "MoneyIncome", id = BaseColumns._ID)
+public class MoneyIncome extends HyjModel{
+
+	@Column(name = "id", index = true, unique = true)
+	private String mId;
+	
+	@Column(name = "pictureId")
+	private String mPictureId;
+	
+	@Column(name = "date")
+	private String mDate;
+
+	@Column(name = "amount")
+	private Double mAmount;
+	
+	@Column(name = "incomeType")
+	private String mIncomeType;
+
+	@Column(name = "friendUserId")
+	private String mFriendUserId;
+
+	@Column(name = "localFriendId")
+	private String mLocalFriendId;
+
+	@Column(name = "friendAccountId")
+	private String mFriendAccountId;
+
+	@Column(name = "moneyAccountId")
+	private String mMoneyAccountId;
+	
+	@Column(name = "projectId")
+	private String mProjectId;
+	
+	@Column(name = "moneyIncomeCategory")
+	private String mMoneyIncomeCategory;
+	
+	@Column(name = "exchangeRate")
+	private Double mExchangeRate;
+
+	@Column(name = "remark")
+	private String mRemark;
+	
+	@Column(name = "serverRecordHash")
+	private String mServerRecordHash;
+
+	@Column(name = "lastServerUpdateTime")
+	private String mLastServerUpdateTime;
+
+	@Column(name = "lastClientUpdateTime")
+	private String mLastClientUpdateTime;
+
+	@Column(name = "lastSyncTime")
+	private String mLastSyncTime;
+
+	@Column(name = "ownerUserId")
+	private String mOwnerUserId;
+
+	@Column(name = "location")
+	private String mLocation;
+	
+	@Column(name = "geoLon")
+	private String mGeoLon;
+	
+	@Column(name = "geoLat")
+	private String mGeoLat;
+	
+	@Column(name = "address")
+	private String mAddress;	
+	
+	public MoneyIncome(){
+		super();
+		mId = UUID.randomUUID().toString();
+	}
+
+	public String getId() {
+		return mId;
+	}
+
+	public void setId(String mId) {
+		this.mId = mId;
+	}
+
+	public String getPictureId() {
+		return mPictureId;
+	}
+
+	public void setPictureId(String mPictureId) {
+		this.mPictureId = mPictureId;
+	}
+
+	public String getDate() {
+		return mDate;
+	}
+
+	public void setDate(String mDate) {
+		this.mDate = mDate;
+	}
+
+	public Double getAmount() {
+		return mAmount;
+	}
+
+	public void setAmount(Double mAmount) {
+		this.mAmount = mAmount;
+	}
+	
+	public String getIncomeType() {
+		return mIncomeType;
+	}
+
+	public void setIncomeType(String mIncomeType) {
+		this.mIncomeType = mIncomeType;
+	}
+
+	public Friend getFriend(){
+		if(mFriendUserId != null){
+			return (Friend) getModel(Friend.class, mFriendUserId);
+		}else if(mLocalFriendId != null){
+			return (Friend) getModel(Friend.class, mLocalFriendId);
+		}
+		return null;
+	}
+	
+	public void setFriend(Friend mFriend) {
+		if(mFriend.getFriendUserId() != null){
+			this.mFriendUserId = mFriend.getId();
+		}
+		else{
+			this.mLocalFriendId = mFriend.getId();
+		}
+	}
+	
+	public String getFriendUserId() {
+		return mFriendUserId;
+	}
+
+	public void setFriendUserId(String mFriendUserId) {
+		this.mFriendUserId = mFriendUserId;
+	}
+
+	public String getLocalFriendId() {
+		return mLocalFriendId;
+	}
+
+	public void setLocalFriendId(String mLocalFriendId) {
+		this.mLocalFriendId = mLocalFriendId;
+	}
+
+	public String getFriendAccountId() {
+		return mFriendAccountId;
+	}
+
+	public void setFriendAccountId(String mFriendAccountId) {
+		this.mFriendAccountId = mFriendAccountId;
+	}
+
+	public MoneyAccount getMoneyAccount(){
+		if(mMoneyAccountId == null){
+			return null;
+		}
+		return (MoneyAccount) getModel(MoneyAccount.class, mMoneyAccountId);
+	}
+	
+	public void setMoneyAccount(MoneyAccount mMoneyAccount) {
+		this.mMoneyAccountId = mMoneyAccount.getId();
+	}
+	
+	public String getMoneyAccountId() {
+		return mMoneyAccountId;
+	}
+
+	public void setMoneyAccountId(String mMoneyAccountId) {
+		this.mMoneyAccountId = mMoneyAccountId;
+	}
+
+	public Project getProject(){
+		if(mProjectId == null){
+			return null;
+		}
+		return (Project) getModel(Project.class, mProjectId);
+	}
+	
+	public void setProject(Project mProject) {
+		this.mProjectId = mProject.getId();
+	}
+	
+	public String getProjectId() {
+		return mProjectId;
+	}
+
+	public void setProjectId(String mProjectId) {
+		this.mProjectId = mProjectId;
+	}
+
+	public String getMoneyIncomeCategory() {
+		return mMoneyIncomeCategory;
+	}
+
+	public void setMoneyIncomeCategory(String mMoneyIncomeCategory) {
+		this.mMoneyIncomeCategory = mMoneyIncomeCategory;
+	}
+
+	public Double getExchangeRate() {
+		return mExchangeRate;
+	}
+
+	public void setExchangeRate(Double mExchangeRate) {
+		this.mExchangeRate = mExchangeRate;
+	}
+
+	public String getRemark() {
+		return mRemark;
+	}
+
+	public void setRemark(String mRemark) {
+		this.mRemark = mRemark;
+	}
+
+	public String getServerRecordHash() {
+		return mServerRecordHash;
+	}
+
+	public void setServerRecordHash(String mServerRecordHash) {
+		this.mServerRecordHash = mServerRecordHash;
+	}
+
+	public String getLastServerUpdateTime() {
+		return mLastServerUpdateTime;
+	}
+
+	public void setLastServerUpdateTime(String mLastServerUpdateTime) {
+		this.mLastServerUpdateTime = mLastServerUpdateTime;
+	}
+
+	public String getLastClientUpdateTime() {
+		return mLastClientUpdateTime;
+	}
+
+	public void setLastClientUpdateTime(String mLastClientUpdateTime) {
+		this.mLastClientUpdateTime = mLastClientUpdateTime;
+	}
+
+	public String getLastSyncTime() {
+		return mLastSyncTime;
+	}
+
+	public void setLastSyncTime(String mLastSyncTime) {
+		this.mLastSyncTime = mLastSyncTime;
+	}
+
+	public String getOwnerUserId() {
+		return mOwnerUserId;
+	}
+
+	public void setOwnerUserId(String mOwnerUserId) {
+		this.mOwnerUserId = mOwnerUserId;
+	}
+
+	public String getLocation() {
+		return mLocation;
+	}
+
+	public void setLocation(String mLocation) {
+		this.mLocation = mLocation;
+	}
+
+	public String getGeoLon() {
+		return mGeoLon;
+	}
+
+	public void setGeoLon(String mGeoLon) {
+		this.mGeoLon = mGeoLon;
+	}
+
+	public String getGeoLat() {
+		return mGeoLat;
+	}
+
+	public void setGeoLat(String mGeoLat) {
+		this.mGeoLat = mGeoLat;
+	}
+
+	public String getAddress() {
+		return mAddress;
+	}
+
+	public void setAddress(String mAddress) {
+		this.mAddress = mAddress;
+	}
+	
+	@Override
+	public void validate(HyjModelEditor modelEditor) {
+		if(this.getDate() == null){
+			modelEditor.setValidationError("date",R.string.moneyIncomeFormFragment_editText_hint_date);
+		}else{
+			modelEditor.removeValidationError("date");
+		}
+//		if(this.getAmount() == null){
+//			modelEditor.setValidationError("amount",R.string.moneyIncomeFormFragment_editText_hint_amount);
+//		}else{
+//			modelEditor.removeValidationError("amount");
+//		}
+//		if(this.getMoneyAccountId() == null){
+//			modelEditor.setValidationError("moneyAccount",R.string.moneyIncomeFormFragment_editText_hint_moneyAccount);
+//		}else{
+//			modelEditor.removeValidationError("moneyAccount");
+//		}
+//		if(this.getProjectId() == null){
+//			modelEditor.setValidationError("project",R.string.moneyIncomeFormFragment_editText_hint_project);
+//		}else{
+//			modelEditor.removeValidationError("project");
+//		}
+//		if(this.getFriend() == null){
+//			modelEditor.setValidationError("friend",R.string.moneyIncomeFormFragment_editText_hint_friend);
+//		}else{
+//			modelEditor.removeValidationError("friend");
+//		}
+	}
+
+}
