6131b70b0e6293987c88a0761b047f07815020d6
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2014-04-16 16:14:26 +0800

    z

--- src/com/hoyoji/hoyoji/money/MoneyTransferFormFragment.java
+++ src/com/hoyoji/hoyoji/money/MoneyTransferFormFragment.java
@@ -588,21 +588,23 @@ public class MoneyTransferFormFragment extends HyjUserFormFragment {
 					newExchange.setRate(moneyTransferModel.getExchangeRate());
 					newExchange.setOwnerUserId(HyjApplication.getInstance().getCurrentUser().getId());
 					newExchange.save();
-				}else if(!localCurrencyId.equalsIgnoreCase(foreignCurrencyId)){
-					Exchange exchange = Exchange.getExchange(localCurrencyId, foreignCurrencyId);
-					Double rate = HyjUtil.toFixed2(moneyTransferModel.getExchangeRate());
-					if(exchange != null){
-						if(exchange.getRate() != rate){
-							HyjModelEditor<Exchange> exchangModelEditor = exchange.newModelEditor();
-							exchangModelEditor.getModelCopy().setRate(rate);
-							exchangModelEditor.save();
-						}
-					}else{
-						exchange = Exchange.getExchange(localCurrencyId, foreignCurrencyId);
-						if(exchange.getRate() != 1/rate){
-							HyjModelEditor<Exchange> exchangModelEditor = exchange.newModelEditor();
-							exchangModelEditor.getModelCopy().setRate(1/rate);
-							exchangModelEditor.save();
+				}else if(localCurrencyId != null && foreignCurrencyId != null){
+					if(localCurrencyId.equalsIgnoreCase(foreignCurrencyId)){
+						Exchange exchange = Exchange.getExchange(localCurrencyId, foreignCurrencyId);
+						Double rate = HyjUtil.toFixed2(moneyTransferModel.getExchangeRate());
+						if(exchange != null){
+							if(exchange.getRate() != rate){
+								HyjModelEditor<Exchange> exchangModelEditor = exchange.newModelEditor();
+								exchangModelEditor.getModelCopy().setRate(rate);
+								exchangModelEditor.save();
+							}
+						}else{
+							exchange = Exchange.getExchange(localCurrencyId, foreignCurrencyId);
+							if(exchange.getRate() != 1/rate){
+								HyjModelEditor<Exchange> exchangModelEditor = exchange.newModelEditor();
+								exchangModelEditor.getModelCopy().setRate(1/rate);
+								exchangModelEditor.save();
+							}
 						}
 					}
 				}
