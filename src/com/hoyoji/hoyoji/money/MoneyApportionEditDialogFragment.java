af30201e913020d465752d00b84f6d7150b2d836
benyiu <szepan.yiu@gmail.com>
2014-09-08 23:20:06 +0800

    fsda

--- src/com/hoyoji/hoyoji/money/MoneyApportionEditDialogFragment.java
+++ src/com/hoyoji/hoyoji/money/MoneyApportionEditDialogFragment.java
@@ -7,14 +7,12 @@ import com.hoyoji.hoyoji_android.R;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 
