a9eeda6fd37f9836eb8f3aa666e75b0d8dba1ba9
bruselee <hoyoji.lijianqiang@gmail.com>
2014-11-20 17:08:31 +0800

    expense invitelink

--- src/com/hoyoji/hoyoji/money/MoneyIncomeContainerFormFragment.java
+++ src/com/hoyoji/hoyoji/money/MoneyIncomeContainerFormFragment.java
@@ -517,6 +517,9 @@ public class MoneyIncomeContainerFormFragment extends HyjUserFormFragment {
 				setExchangeRate(false);
 				this.getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 			}else{
+				if(moneyIncomeContainer.getOwnerUserId() != HyjApplication.getInstance().getCurrentUser().getId()) {
+					app_action_save_template.setVisibility(View.GONE);
+				}
 				setExchangeRate(true);
 			}
 			setPermission();
