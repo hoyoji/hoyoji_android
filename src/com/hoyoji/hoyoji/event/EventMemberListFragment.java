4f714eb08e5049f234cfed3c1f0bff10ecca6242
bruselee <hoyoji.lijianqiang@gmail.com>
2014-12-17 17:32:23 +0800

    e

--- src/com/hoyoji/hoyoji/event/EventMemberListFragment.java
+++ src/com/hoyoji/hoyoji/event/EventMemberListFragment.java
@@ -316,18 +316,30 @@ public class EventMemberListFragment extends HyjUserListFragment {
 			return true;
 		}
 		if(item.getItemId() == R.id.projectEventMemberListFragment_action_add){
+			if(mIsSelectSignInEventMembers = true){
+				
+			}
 			Bundle bundle = new Bundle();
 			bundle.putLong("EVENT_ID", modelId);
 			openActivityWithFragment(EventMemberFormFragment.class, R.string.projectEventMemberFormFragment_action_addnew, bundle);
 			return true;
 		} else if(item.getItemId() == R.id.projectEventMemberListFragment_action_member_invite){
-			inviteFriend("Other", event, event.getName());
+			inviteFriend("Other", event, event.getName(), "invite");
 			return true;
 		} else if(item.getItemId() == R.id.projectEventMemberListFragment_action_member_invite_wxFriend){
-			inviteFriend("WX", event, event.getName());
+			inviteFriend("WX", event, event.getName(), "invite");
 			return true;
 		} else if(item.getItemId() == R.id.projectEventMemberListFragment_action_member_invite_qqFriend){
-			inviteFriend("QQ", event, event.getName());
+			inviteFriend("QQ", event, event.getName(), "invite");
+			return true;
+		} else if(item.getItemId() == R.id.projectEventMemberListFragment_action_member_signIn){
+			inviteFriend("Other", event, event.getName(), "signIn");
+			return true;
+		} else if(item.getItemId() == R.id.projectEventMemberListFragment_action_member_signIn_wxFriend){
+			inviteFriend("WX", event, event.getName(), "signIn");
+			return true;
+		} else if(item.getItemId() == R.id.projectEventMemberListFragment_action_member_signIn_qqFriend){
+			inviteFriend("QQ", event, event.getName(), "signIn");
 			return true;
 		}
 //		else if(item.getItemId() == R.id.projectEventMemberListFragment_action_member_edit){
@@ -352,9 +364,19 @@ public class EventMemberListFragment extends HyjUserListFragment {
 		return super.onOptionsItemSelected(item);
 	}
 	
-	public void inviteFriend(final String way, Event event,final String event_name) {
+	public void inviteFriend(final String way, Event event,final String event_name, final String type) {
 		((HyjActivity) getActivity()).displayProgressDialog(R.string.friendListFragment__action_invite_title,R.string.friendListFragment__action_invite_content);
-		
+		String emTitle = null;
+		String emDescription = null;
+		if(type.equals("invite")){
+			emTitle = "邀请参加活动";
+			emDescription = HyjApplication.getInstance().getCurrentUser().getDisplayName() + " 邀请您参加活动    " +event_name;
+		} else if(type.equals("signIn")){
+			emTitle = "活动签到";
+			emDescription = HyjApplication.getInstance().getCurrentUser().getDisplayName() + " 邀请您进行活动    " +event_name +"签到";
+		}
+		final String emTitleSent = emTitle;
+		final String emDescriptionSent = emDescription;
 		
 		JSONObject inviteFriendObject = new JSONObject();
 		final String id = UUID.randomUUID().toString();
@@ -362,10 +384,10 @@ public class EventMemberListFragment extends HyjUserListFragment {
 			inviteFriendObject.put("id", id);
 			inviteFriendObject.put("data", event.toJSON().toString());
 			inviteFriendObject.put("__dataType", "InviteLink");
-			inviteFriendObject.put("title", "邀请参加活动");
+			inviteFriendObject.put("title", emTitle);
 			inviteFriendObject.put("type", "EventMember");
 			inviteFriendObject.put("date", (new Date()).getTime());
-			inviteFriendObject.put("description", HyjApplication.getInstance().getCurrentUser().getDisplayName() + " 邀请您参加活动    " +event_name);
+			inviteFriendObject.put("description", emDescription);
 			inviteFriendObject.put("state", "Open");
 		} catch (JSONException e1) {
 			e1.printStackTrace();
@@ -376,12 +398,18 @@ public class EventMemberListFragment extends HyjUserListFragment {
 			@Override
 			public void finishCallback(Object object) {
 				((HyjActivity) getActivity()).dismissProgressDialog();
+				String linkUrl = null;
+				if(type.equals("invite")){
+					linkUrl = HyjApplication.getInstance().getServerUrl()+"m/invite.html?id=" + id;
+				} else if(type.equals("signIn")){
+					linkUrl = HyjApplication.getInstance().getServerUrl()+"m/eventSignIn.html?id=" + id;
+				}
 					if(way.equals("Other")){
-						inviteOtherFriend(id,event_name);
+						inviteOtherFriend(linkUrl, event_name, emTitleSent, emDescriptionSent);
 					} else if(way.equals("WX")){
-						inviteWXFriend(id,event_name);
+						inviteWXFriend(linkUrl, event_name, emTitleSent, emDescriptionSent);
 					} else if(way.equals("QQ")){
-						inviteQQFriend(id,event_name);
+						inviteQQFriend(linkUrl, event_name, emTitleSent, emDescriptionSent);
 					}
 			}
 
@@ -402,7 +430,7 @@ public class EventMemberListFragment extends HyjUserListFragment {
    	 	HyjHttpPostAsyncTask.newInstance(serverCallbacks, "[" + inviteFriendObject.toString() + "]", "postData");
 	 }
 
-	public void inviteOtherFriend(String id,String event_name) {
+	public void inviteOtherFriend(String linkUrl, String event_name, String emTitleSent, String emDescriptionSent) {
 		Intent intent=new Intent(Intent.ACTION_SEND);   
         intent.setType("text/plain");   
         
@@ -421,21 +449,21 @@ public class EventMemberListFragment extends HyjUserListFragment {
 //		} catch (IOException e) {
 //			e.printStackTrace();
 //		}
-	        intent.putExtra(Intent.EXTRA_TITLE, "邀请参加活动");  
-	        intent.putExtra(Intent.EXTRA_SUBJECT, "邀请参加活动");   
-	        intent.putExtra(Intent.EXTRA_TEXT, HyjApplication.getInstance().getCurrentUser().getDisplayName() + " 邀请您参加活动"+event_name+"。\n\n" + HyjApplication.getServerUrl()+"m/invite.html?id=" + id);  
+	        intent.putExtra(Intent.EXTRA_TITLE, emTitleSent);  
+	        intent.putExtra(Intent.EXTRA_SUBJECT, emTitleSent);   
+	        intent.putExtra(Intent.EXTRA_TEXT, HyjApplication.getInstance().getCurrentUser().getDisplayName() + emTitleSent +event_name+"。\n\n" + linkUrl);  
 	        
 	        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);   
-	        startActivity(Intent.createChooser(intent, "邀请参加活动")); 
+	        startActivity(Intent.createChooser(intent, emTitleSent)); 
 	}
 	
-	public void inviteWXFriend(String id,String event_name) {
+	public void inviteWXFriend(String linkUrl,String event_name, String emTitleSent, String emDescriptionSent) {
 		api = WXAPIFactory.createWXAPI(getActivity(), AppConstants.WX_APP_ID);
 		WXWebpageObject webpage = new WXWebpageObject();
-		webpage.webpageUrl = HyjApplication.getInstance().getServerUrl()+"m/invite.html?id=" + id;
+		webpage.webpageUrl = linkUrl;
 		WXMediaMessage msg = new WXMediaMessage(webpage);
-		msg.title = "邀请参加活动";
-		msg.description = HyjApplication.getInstance().getCurrentUser().getDisplayName() + " 邀请您参加活动" + event_name + "。";
+		msg.title = emTitleSent;
+		msg.description = emDescriptionSent;
 		Bitmap thumb = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
 		msg.thumbData = Util.bmpToByteArray(thumb, true);
 		
@@ -451,12 +479,12 @@ public class EventMemberListFragment extends HyjUserListFragment {
 		return (type == null) ? String.valueOf(System.currentTimeMillis()) : type + System.currentTimeMillis();
 	}
 	
-	public void inviteQQFriend(String id,String event_name) {
+	public void inviteQQFriend(String linkUrl,String event_name, String emTitleSent, String emDescriptionSent) {
 		final Bundle params = new Bundle();
 	    params.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
-	    params.putString(QQShare.SHARE_TO_QQ_TITLE, "邀请参加活动");
-	    params.putString(QQShare.SHARE_TO_QQ_SUMMARY,  HyjApplication.getInstance().getCurrentUser().getDisplayName() + " 邀请您参加活动"+event_name+"。");
-	    params.putString(QQShare.SHARE_TO_QQ_TARGET_URL,  HyjApplication.getInstance().getServerUrl()+"m/invite.html?id=" + id);
+	    params.putString(QQShare.SHARE_TO_QQ_TITLE, emTitleSent);
+	    params.putString(QQShare.SHARE_TO_QQ_SUMMARY,  emDescriptionSent);
+	    params.putString(QQShare.SHARE_TO_QQ_TARGET_URL,  linkUrl);
 	    params.putString(QQShare.SHARE_TO_QQ_IMAGE_URL, HyjApplication.getInstance().getServerUrl() + "imgs/invite_friend.png");
 	    params.putString(QQShare.SHARE_TO_QQ_APP_NAME,  "好友AA记账");
 //	    params.putInt(QQShare.SHARE_TO_QQ_EXT_INT,  "其他附加功能");		
