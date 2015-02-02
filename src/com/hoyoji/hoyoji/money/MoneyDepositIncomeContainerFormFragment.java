818dec2c719012cfeef6528c193222c732623881
bruselee <hoyoji.lijianqiang@gmail.com>
2015-02-02 16:10:42 +0800

    fixed #607
    账务上的一些需更改的字眼
    
    fixed #622
    
    转账页面上方加一行说明：转账不计算收支，只是资产调拨

--- src/com/hoyoji/hoyoji/money/MoneyDepositIncomeContainerFormFragment.java
+++ src/com/hoyoji/hoyoji/money/MoneyDepositIncomeContainerFormFragment.java
@@ -387,7 +387,7 @@ public class MoneyDepositIncomeContainerFormFragment extends HyjUserFormFragment
 					bundle.putLong("MODEL_ID", project.get_mId());
 					bundle.putString("NULL_ITEM", (String)mSelectorFieldFinancialOwner.getHint());
 					bundle.putBoolean("disableMultiChoiceMode", true);
-					openActivityWithFragmentForResult(ProjectMemberListFragment.class, R.string.friendListFragment_title_select_friend_creditor, bundle, GET_FINANCIALOWNER_ID);
+					openActivityWithFragmentForResult(ProjectMemberListFragment.class, R.string.explainFinancialOwnerFragment_title, bundle, GET_FINANCIALOWNER_ID);
 				}
 			}
 		});
